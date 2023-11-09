61aba91d18e904efbe26dbd83da94983108e3013
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-11-09 15:15:45 +0900

    :bug: Fix : 데이터베이스 네임 읽히지 않는 문제

--- Server/my-project/config/database.js
+++ Server/my-project/config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => {
       connection: {
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
+        database: env("DATABASE_NAME", "kimmandoo"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
         ssl: env.bool("DATABASE_SSL", false),
