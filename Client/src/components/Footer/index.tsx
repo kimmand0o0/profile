ace72e7683fb4d3660fbd7cfc3b776e7d4c558bc
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-11-09 16:59:43 +0900

    :lipstick: Design : 푸터 bg-color 추가

--- Client/src/components/Footer/index.tsx
+++ Client/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import Contact from './Contact';
 const Footer = () => {
   return (
     <>
-      <footer className="w-screen fixed inset-x-0 bottom-0 z-50 left-0 bg-transparent text-gray-700 body-font">
+      <footer className="w-screen bg-white fixed inset-x-0 bottom-0 z-50 left-0 bg-transparent text-gray-700 body-font">
         <div className="max-w-screen-md p-2 container mx-auto flex flex-wrap flex-row justify-between items-center">
           <Contact />
           <Buttons />
