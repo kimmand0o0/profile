90d09287cddd43f72c6744c8473798627afa7d6e
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-11-14 14:01:50 +0900

    :zap: Feat : 데이터 정리

--- Client/src/components/AboutMeEducation/index.tsx
+++ Client/src/components/AboutMeEducation/index.tsx
@@ -11,16 +11,102 @@ const AboutMeEducation = () => {
               <p className="font-bold text-slate-600">코드스테이츠 45기 수료</p>
               <p className="text-sm text-slate-400">2023.04 - 2023.10</p>
             </div>
-            <p className="text-sm text-slate-600 pl-1">
-              - 프론트엔드 React 과정
-            </p>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">
+                - 프론트엔드 React 과정
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                React.js를 사용하여 운동 루틴을 투두리스트로 기록하고 관리할 수
+                있는 기능을 개발하였습니다. 사용자 친화적인 UI/UX를 고려하여
+                프로젝트를 디자인하고 구현했습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">- 커뮤니티 구현</p>
+              <p className="text-xs text-slate-600 pl-4">
+                소셜 요소를 추가하여 사용자들 간에 운동 루틴을 공유하고 소통할
+                수 있는 커뮤니티 기능을 구현했습니다. 사용자들 간의 경험을
+                공유하고 자극을 받을 수 있는 기능을 추가하여 프로젝트의 가치를
+                향상시켰습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">
+                - 멘토님과의 코드 리뷰 및 리팩토링
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                멘토님과의 코드 리뷰를 통해 코드의 가독성, 유지보수성, 성능 등에
+                대한 피드백을 받았습니다. 이를 기반으로 리팩토링을 진행하여
+                코드의 품질을 향상시키고 개선점을 찾아나갔습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">
+                - 비즈니스 로직 개선
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                코드 리뷰를 통해 비즈니스 로직에 대한 고민을 하고, 효율적이고
+                안정적인 코드를 작성하는 방법에 대한 경험을 얻었습니다.
+                리팩토링을 통해 지속적인 성능 개선과 사용자 경험 향상에
+                기여했습니다.
+              </p>
+            </div>
           </div>
           <div className="p-2">
             <div className="w-full flex md:flex-row flex-col justify-between">
               <p className="font-bold text-slate-600">항해99 10기 수료</p>
               <p className="text-sm text-slate-400">2022.11 - 2023.02</p>
             </div>
-            <p className="text-sm text-slate-600 pl-1">- 백엔드 Node 과정 </p>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">- 백엔드 Node 과정</p>
+              <p className="text-xs text-slate-600 pl-4">
+                Node.js를 활용한 백엔드 개발과정을 수료하였으며, 메인
+                프로젝트에서는 디자이너와 프론트엔드 팀과의 협업을 통해 약학
+                데이터를 시각적으로 보여주는 프로젝트를 성공적으로 진행했습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">
+                - 약학 데이터 프로젝트
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                공공데이터를 수집하고 가공하여 약학 데이터를 시각적으로 보여주는
+                프로젝트를 주도적으로 진행했습니다.
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                OAuth를 활용하여 로그인 기능을 구현하였고, 사용자의 편리한
+                경험을 고려하여 설계했습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">- 프로젝트 MVP 달성</p>
+              <p className="text-xs text-slate-600 pl-4">
+                프로젝트에서 목표로 한 MVP를 100% 완수하여 우수한 성과를
+                얻었습니다.
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                사용자들에게 편리하고 가치 있는 기능을 제공하여 프로젝트를
+                성공적으로 마무리했습니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">- 프로젝트상 수상</p>
+              <p className="text-xs text-slate-600 pl-4">
+                프로젝트의 품질과 성과가 높게 평가되어 최고의 프로젝트상을
+                수상하였습니다. 이는 팀 전체의 노력과 협업의 결과로 이루어진
+                성과입니다.
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-sm text-slate-600 pl-1">
+                - 원활한 의사소통과 협업
+              </p>
+              <p className="text-xs text-slate-600 pl-4">
+                동료들과의 원활한 의사소통을 통해 팀원으로 신뢰받고 함께할 수
+                있는 '든든한 국밥'상을 수상하였습니다. 팀 내에서의 협업과 소통
+                능력이 인정받은 결과입니다.
+              </p>
+            </div>
           </div>
           <div className="p-2">
             <div className="w-full flex md:flex-row flex-col justify-between">
--- Client/src/components/AboutMeIntroduction/index.tsx
+++ Client/src/components/AboutMeIntroduction/index.tsx
@@ -2,18 +2,31 @@ const AboutMeIntroduction = () => {
   return (
     <>
       <div className="pt-12 pb-10 px-4 border-b border-slate-300 flex flex-col text-slate-800 font-[GmarketSansMedium] ">
-        <span className="m-1">
-          ✔️ 문제와 긍정적으로 마주하며, 상황에 적합한 해결 방법인지를 계속해서
-          생각합니다.
-        </span>
-        <span className="m-1">
-          ✔️ 스트레스와 컨디션을 잘 관리하여 프로젝트를 완주해내는 능력이
-          있습니다.
-        </span>
-        <span className="m-1">
-          ✔️ Git Convention과, Pull requests 리뷰를 통해 동료와 발전해
-          나갔습니다.
-        </span>
+        <div className="py-2">
+          <p className="m-1">✔️ 문제 해결과 긍정적인 태도</p>
+          <p className="m-1 text-sm">
+            어떤 문제도 긍정적으로 받아들이고, 그에 대한 최적의 해결책을 찾기
+            위해 노력합니다. 이를 통해 어떠한 도전이라도 성공으로 이끌어내는
+            것이 제 강점 중 하나입니다.
+          </p>
+        </div>
+        <div className="py-2">
+          <p className="m-1">✔️ 스트레스 관리 및 프로젝트 완주</p>
+          <p className="m-1 text-sm">
+            프로젝트를 완주하기 위해 스트레스와 컨디션을 철저히 관리합니다. 고객
+            서비스 기록 웹 사이트를 기획하고 개발한 경험에서 나온 높은 책임감과
+            개발을 즐거워하는 스트레스 관리 능력이, 어떠한 상황에서도 최상의
+            결과를 이끌어내는 데에 큰 역할을 했습니다.
+          </p>
+        </div>
+        <div className="py-2">
+          <p className="m-1">✔️ Git Convention 준수와 협업</p>
+          <p className="m-1 text-sm">
+            Git Convention을 엄수하며, Pull requests 리뷰를 통해 동료들과 함께
+            성장한 경험이 있습니다. 이는 팀 프로젝트에서의 원활한 협업과
+            효율적인 코드 관리에 기여했습니다.
+          </p>
+        </div>
       </div>
     </>
   );
