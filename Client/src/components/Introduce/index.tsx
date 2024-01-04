8d5f1de0b50c4a835d61a118e74b70ac8d07d7d7
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-01-05 01:58:55 +0900

    :art: Fix : 이미지 압축

--- Client/src/components/Introduce/index.tsx
+++ Client/src/components/Introduce/index.tsx
@@ -1,4 +1,3 @@
-// import BuyMeModal from 'components/BuyMeModal';
 import Typing from './Typing';
 
 import me from '../../assets/images/me.png';
@@ -9,7 +8,6 @@ const Introduce = () => {
       <div className="w-full pt-14 px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="w-full flex flex-col">
           <Typing />
-          {/* <BuyMeModal /> */}
         </div>
         <img src={me} alt="me" className="w-1/4 mr-3 hidden md:block" />
       </div>
